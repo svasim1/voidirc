9b5105e380ea2e4001ccb940546428cb955ae85f
svasim <svante.ericsson06@gmail.com>
2025-10-01 13:14:29 +0200

    initialize Koa server with WebSocket support; update package.json and package-lock.json with necessary dependencies; modify .gitignore for temporary files

---
+++ app.js
@@ -0,0 +1,77 @@
+import Koa from 'koa';
+import websocket from 'koa-websocket';
+import serve from 'koa-static';
+
+const koa = new Koa();
+const app = websocket(koa);
+
+// HTTP status codes
+const HTTP_OK = 200;
+const HTTP_INVALID = 400;
+const HTTP_NOTFOUND = 404;
+const HTTP_INTERNAL = 500;
+
+// startup/shutdown of server
+const port = 8080;
+const serverstarttime = Date.now();
+let server = null;
+
+// in-memory storage for users and rooms
+const users = new Map();
+const rooms = new Map();
+let activeRooms = new Set(['general']); 
+
+// serve error page
+async function errorPage(ctx) {
+    ctx.redirect('/error.html');
+}
+
+// serve index page
+koa.use(async (ctx, next) => {
+    if (ctx.path === '/') {
+        ctx.redirect('/index.html');
+    } else {
+        await next();
+    }
+});
+
+// serve static pages
+koa.use(serve('./static'));
+
+// 404 handler
+koa.use(async (ctx) => {
+    await errorPage(ctx);
+});
+
+server = koa.listen(port);
+console.log(`Server started on port ${port}`);
+
+
+// websocket
+app.ws.use(async(ctx) => {
+    console.log('WebSocket connection established');
+
+    // handle messages
+    ctx.websocket.on('message', (message) => {
+        let parsed;
+
+        // parse JSON message
+        try {
+        parsed = JSON.parse(message);
+        console.log("Parsed message:", parsed);
+        } catch (error) {
+            console.error('Error parsing WebSocket message:', error);
+        }
+
+        if (parsed.type === 'NICK') // {"type": "NICK", "data": "<name>"}
+            handleNick(ctx, parsed);
+        else if (parsed.type === 'JOIN') // {"type": "JOIN", "data": "<room>"}
+            handleJoin(ctx, parsed);
+        else if (parsed.type === 'MSG') // {"type": "MSG", "data": "<message>"}
+            handleMsg(ctx, parsed);
+        else if (parsed.type === 'LIST')
+            ctx.websocket.send(JSON.stringify({ type: 'LIST', rooms: Array.from(activeRooms) }));
+        else if (parsed.type === 'QUIT') // {"type": "QUIT"}
+            ctx.websocket.close();
+    });
+});
