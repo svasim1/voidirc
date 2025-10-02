d6ffe9c345f3c9ad5ed96f2bfb1609916ea016b1
FelleCool <slagbrand.felix@gmail.com>
2025-10-02 09:07:29 +0200

    handlers for join, msg and nick

---
+++ handlers/join.js
@@ -0,0 +1,26 @@
+module.exports = function handleJoin(ctx, parsed, activeRooms) {
+    const room = parsed.data;
+
+    if (!room) {
+        ctx.websocket.send(JSON.stringify({ 
+            type: 'ERROR', 
+            message: 'Room name required.' 
+        }));
+        return;
+    }
+
+    // Lägg till ctx i rummet
+    if (!activeRooms.has(room)) {
+        activeRooms.set(room, new Set());
+    }
+
+    activeRooms.get(room).add(ctx);
+    ctx.room = room;
+
+    ctx.websocket.send(JSON.stringify({
+        type: 'JOIN',
+        message: `Joined room ${room}`
+    }));
+
+    console.log(`${ctx.nick || "Anonymous"} joined room ${room}`);
+};
---
+++ handlers/msg.js
@@ -0,0 +1,27 @@
+module.exports = function handleMsg(ctx, parsed, activeRooms) {
+    const message = parsed.data; 
+    const room = ctx.room;
+    const nick = ctx.nick || "Anonymous";
+
+    if (!room) {
+        ctx.websocket.send(JSON.stringify({ 
+            type: 'ERROR', 
+            message: 'You must join a room before sending messages.' 
+        }));
+        return;
+    }
+
+    // Broadcast till alla i rummet
+    for (const clientCtx of activeRooms.get(room) || []) {
+        if (clientCtx.websocket.readyState === 1) { // 1 = OPEN
+            clientCtx.websocket.send(JSON.stringify({
+                type: 'MSG',
+                from: nick,
+                data: message,
+                room: room
+            }));
+        }
+    }
+
+    console.log(`[${room}] ${nick}: ${message}`);
+};
