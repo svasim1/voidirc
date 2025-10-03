edc340de24b9c52a05b6578095841747e9f675d3
FelleCool <slagbrand.felix@gmail.com>
2025-10-03 10:39:47 +0200

    added so you only can be in one rum and so you cant have the same name as a nother

--- handlers/nick.js
+++ handlers/nick.js
@@ -9,6 +9,16 @@ export default function handleNick(ctx, parsed) {
         return;
     }
 
+    for (const user of users.values()) {
+        if (user.nick === nick) {
+            ctx.websocket.send(JSON.stringify({ 
+            type: 'ERROR', 
+            message: 'Nick cannot be the same as somene else.' 
+            }));
+            return;
+        }
+    }
+
     ctx.nick = nick; // Spara nick i ctx
     ctx.websocket.send(JSON.stringify({
         type: 'NICK',
